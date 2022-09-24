6c02b16c2c2b0b3de3877fb632828400d8857239
Gafur Hayybayev <185112007@kocaeli.edu.tr>
2022-09-24 22:57:13 +0300

    using custom display name

--- spring-boot-unit-test/_01-starting-project/src/test/java/com/gafur/junitdemo/DemoUtilsTest.java
+++ spring-boot-unit-test/_01-starting-project/src/test/java/com/gafur/junitdemo/DemoUtilsTest.java
@@ -11,9 +11,28 @@ class DemoUtilsTest {
     @BeforeEach
     void setupBeforeEach(){
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEch executes before the execution of each test method");
     }
 
+    @Test
+    @DisplayName("Equals and Not Equals")
+    void testEqualsAndNotEquals(){
+
+        assertEquals(6, demoUtils.add(2,4), "2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull(){
+
+        String str1 = null;
+        String str2 = "anything";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+    }
+
+    /*
     @AfterEach
     void tearDownAfterEach(){
         System.out.println("Running @AfterEach");
@@ -29,24 +48,5 @@ class DemoUtilsTest {
     @AfterAll
     static void tearDownAfterAll(){
         System.out.println("@AfterAll executes only after all test methods execution in the class");
-    }
-
-    @Test
-    void testEqualsAndNotEquals(){
-        System.out.println("Running test: testEqualsAndNotEquals");
-
-        assertEquals(6, demoUtils.add(2,4), "2 + 4 must be 6");
-        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
-    }
-
-    @Test
-    void testNullAndNotNull(){
-        System.out.println("Running test: testNullAndNotNull");
-
-        String str1 = null;
-        String str2 = "anything";
-
-        assertNull(demoUtils.checkNull(str1), "Object should be null");
-        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
-    }
+    }*/
 }
