0d26d4ec2b7b544e61aaa4bb29493996b2984ad2
Gafur Hayytbayev <185112007@kocaeli.edu.tr>
2022-09-30 10:27:33 +0300

    ordering test methods: order by display name

--- spring-boot-unit-test/_01-starting-project/src/test/java/com/gafur/junitdemo/DemoUtilsTest.java
+++ spring-boot-unit-test/_01-starting-project/src/test/java/com/gafur/junitdemo/DemoUtilsTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
