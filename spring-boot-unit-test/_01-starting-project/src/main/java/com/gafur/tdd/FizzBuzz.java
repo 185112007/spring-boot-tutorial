e5ebddb3428920002e19b9bb5069c160ac509971
Gafur Hayytbayev <185112007@kocaeli.edu.tr>
2022-10-01 08:48:22 +0300

    alternative implementation of FizzBuzz project

--- spring-boot-unit-test/_01-starting-project/src/main/java/com/gafur/tdd/FizzBuzz.java
+++ spring-boot-unit-test/_01-starting-project/src/main/java/com/gafur/tdd/FizzBuzz.java
@@ -6,18 +6,33 @@ public class FizzBuzz {
     // If number is divisible by 5, print Buzz
     // If number is divisible by 3 and 5, print FizzBuzz
     // If number is NOT divisible by 3 or 5, then print the number
+//    public static String compute(int i) {
+//        if ((i % 3 == 0) && (i % 5 == 0)){
+//            return "FizzBuzz";
+//        }
+//        else if (i % 3 == 0){
+//            return "Fizz";
+//        }
+//        else if (i % 5 == 0){
+//            return "Buzz";
+//        }
+//        else {
+//            return Integer.toString(i);
+//        }
+//    }
+
+    // alternative implementation
     public static String compute(int i) {
-        if ((i % 3 == 0) && (i % 5 == 0)){
-            return "FizzBuzz";
+        StringBuilder result = new StringBuilder();
+        if (i % 3 == 0){
+            result.append("Fizz");
         }
-        else if (i % 3 == 0){
-            return "Fizz";
+        if (i % 5 == 0){
+            result.append("Buzz");
         }
-        else if (i % 5 == 0){
-            return "Buzz";
+        if (result.length() == 0){
+            result.append(i);
         }
-        else {
-            return Integer.toString(i);
-        }
+        return result.toString();
     }
 }
