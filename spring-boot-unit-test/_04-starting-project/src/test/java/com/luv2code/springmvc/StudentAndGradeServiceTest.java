86e568e514e4c2cdb7a08969ebe694f17b5cacee
Gafur Hayytbayev <185112007@kocaeli.edu.tr>
2022-10-02 08:51:39 +0300

    testing service layer

---
+++ spring-boot-unit-test/_04-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -0,0 +1,31 @@
+package com.luv2code.springmvc;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@TestPropertySource("/application.properties")
+@SpringBootTest
+public class StudentAndGradeServiceTest {
+
+    @Autowired
+    StudentAndGradeService studentService;
+
+    @Autowired
+    StudentDao studentDao;
+
+    @Test
+    void createStudentService(){
+        studentService.createStudent("Chad", "Darby", "chad.darby@gmail.com");
+
+        CollegeStudent student = studentDao.findByEmailAddress("chad.darby@gmail.com");
+
+        assertEquals("chad.darby@gmail.com", student.getEmailAddress(), "find by email");
+    }
+}
